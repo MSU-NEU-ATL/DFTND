969fd8335bad7e1f525bd2daf2609c6ee27b1c06
LinLabNEU <zhaopuglory@gmail.com>
2020-10-24 19:39:10 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+def main():
+
+    if '--attack_type' in sys.argv:
+        a = sys.argv.index('--attack_type')
+        attack_type = sys.argv[a+1]
+        sys.argv[0] = attack_type + '.py'
+        del sys.argv[a]
+        del sys.argv[a]
+        command = ' '.join(sys.argv[:])
+        command = 'python ' + command
+        print(command)
+        os.system(command)
+    else:
+        print('attack type error. Please provide attack type.')
+    # if len(sys.argv) > 1:
+    #     command = ' '.join(sys.argv[:])
+    # else:
+    #     command = str(sys.argv[0])
+    # print(command)
+
+if __name__ == '__main__':
+    main()
