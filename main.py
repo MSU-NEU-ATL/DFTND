0b2a07fe028e6d2ac1c44904d0b3c4255a92a53f
Yeez-lee <56846630+Yeez-lee@users.noreply.github.com>
2020-11-09 22:23:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,25 @@ import sys
 import os
 
 def main():
-
-    if '--attack_type' in sys.argv:
+    if '--area' in sys.argv and '--attack_type' in sys.argv:
         a = sys.argv.index('--attack_type')
-        attack_type = sys.argv[a+1]
-        if attack_type == 'PGD' or 'FGSM' or 'CW':
-            attack_type = 'PGD'
-        if attack_type == 'sturctured' or 'ADMM':
-            attack_type = 'sturctured'
-        sys.argv[0] = attack_type + '.py'
+        attack_type = sys.argv[a + 1]
+
+        if attack_type == 'PGD_linf' or 'PGD_l2':  # or 'FGSM' or 'CW':
+            attack = 'PGD'
+        sys.argv[0] = attack + '.py'
+
+        if attack_type == 'Patch':
+            attack = 'Patch_Attack'
+        sys.argv[0] = attack + '.py'
 
         command = ' '.join(sys.argv[:])
         command = 'python ' + command
         print(command)
-        os.system(command)
+        # os.system(command)
     else:
         print('attack type error. Please provide attack type.')
-    # if len(sys.argv) > 1:
-    #     command = ' '.join(sys.argv[:])
-    # else:
-    #     command = str(sys.argv[0])
-    # print(command)
+
 
 if __name__ == '__main__':
     main()
